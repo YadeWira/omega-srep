2333a966e51f68c41021e9883db65b525a1d1fcd
YadeWira <sebasgamerelpro@gmail.com>
2026-04-27 08:51:38 -0500

    Threads.c: skip unshipped ThreadsWin32.c on MinGW (matches Threads.h gate)

--- Compression/LZMA2/C/Threads.c
+++ Compression/LZMA2/C/Threads.c
@@ -1,4 +1,6 @@
-#ifdef _WIN32
+// MinGW: see comment in Threads.h. ThreadsWin32.c was never shipped
+// with this fork; MinGW uses the POSIX path via winpthreads.
+#if defined(_WIN32) && !defined(__MINGW32__)
 #include "ThreadsWin32.c"
 #else
 #include "ThreadsUnix.c"
